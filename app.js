e4d50b1182d186ce11aa4e51eeee305cf3b9ccd4
Sebastian Flor <sflor@telecomboutique.com>
2020-11-17 20:54:01 +0100

    feat(app): enums

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-/* OBJECTS, ARRAYS & TUPLES */
+/* OBJECTS, ARRAYS, TUPLES, ENUMS */
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];
+// } = {
+//     name: 'Sebastian',
+//     age: 23,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']
+// };
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
 var person = {
     name: 'Sebastian',
     age: 23,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    role: Role.ADMIN
 };
 var favoriteActivities;
 favoriteActivities = ['Sports'];
@@ -11,5 +29,8 @@ for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
     //console.log(hobby.map()); // ERROR!!
+}
+if (person.role === Role.ADMIN) {
+    console.log('is admin!');
 }
 console.log(person);
--- app.ts
+++ app.ts
@@ -1,15 +1,24 @@
-/* OBJECTS, ARRAYS & TUPLES */
+/* OBJECTS, ARRAYS, TUPLES, ENUMS */
 
-const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string];
-} = {
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];
+// } = {
+//     name: 'Sebastian',
+//     age: 23,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']
+// };
+
+enum Role {ADMIN, READ_ONLY, AUTHOR};
+
+const person = {
     name: 'Sebastian',
     age: 23,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    role: Role.ADMIN
 };
 
 let favoriteActivities: string[];
@@ -18,6 +27,11 @@ favoriteActivities = ['Sports'];
 for(const hobby of person.hobbies){
     console.log(hobby.toUpperCase());
     //console.log(hobby.map()); // ERROR!!
+}
+
+if(person.role === Role.ADMIN){
+    console.log('is admin!');
+    
 }
 
 console.log(person);
