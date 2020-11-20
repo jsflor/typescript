81f8ac88547e87f90f13e38e30c4e79558656828
Sebastian Flor <sflor@telecomboutique.com>
2020-11-20 13:07:48 +0100

    feat: union and literal types

--- app.js
+++ app.js
@@ -1,36 +1,17 @@
-/* OBJECTS, ARRAYS, TUPLES, ENUMS */
-// const person: {
-//     name: string;
-//     age: number;
-//     hobbies: string[];
-//     role: [number, string];
-// } = {
-//     name: 'Sebastian',
-//     age: 23,
-//     hobbies: ['Sports', 'Cooking'],
-//     role: [2, 'author']
-// };
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-;
-var person = {
-    name: 'Sebastian',
-    age: 23,
-    hobbies: ['Sports', 'Cooking'],
-    role: Role.ADMIN
-};
-var favoriteActivities;
-favoriteActivities = ['Sports'];
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
-    //console.log(hobby.map()); // ERROR!!
+function combine(input1, input2, resultConversion) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    // if(resultConversion === 'as-number'){
+    //     return +result;
+    // } else {
+    //     return result.toString();
+    // }
+    return result;
 }
-if (person.role === Role.ADMIN) {
-    console.log('is admin!');
-}
-console.log(person);
+console.log(combine(2, 4, 'as-number'));
+console.log(combine('Juan', 'Sebastian', 'as-text'));
--- app.ts
+++ app.ts
@@ -1,37 +1,20 @@
-/* OBJECTS, ARRAYS, TUPLES, ENUMS */
-
-// const person: {
-//     name: string;
-//     age: number;
-//     hobbies: string[];
-//     role: [number, string];
-// } = {
-//     name: 'Sebastian',
-//     age: 23,
-//     hobbies: ['Sports', 'Cooking'],
-//     role: [2, 'author']
-// };
-
-enum Role {ADMIN, READ_ONLY, AUTHOR};
-
-const person = {
-    name: 'Sebastian',
-    age: 23,
-    hobbies: ['Sports', 'Cooking'],
-    role: Role.ADMIN
-};
-
-let favoriteActivities: string[];
-favoriteActivities = ['Sports'];
-
-for(const hobby of person.hobbies){
-    console.log(hobby.toUpperCase());
-    //console.log(hobby.map()); // ERROR!!
-}
-
-if(person.role === Role.ADMIN){
-    console.log('is admin!');
+function combine(input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-text'){
+    let result;
     
+    if(typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number'){
+        result = +input1 + +input2;
+    } else {
+        result = input1.toString() + input2.toString();
+    }
+    
+    // if(resultConversion === 'as-number'){
+    //     return +result;
+    // } else {
+    //     return result.toString();
+    // }
+
+    return result;
 }
 
-console.log(person);
+console.log(combine(2, 4, 'as-number'));
+console.log(combine('Juan', 'Sebastian', 'as-text'));
