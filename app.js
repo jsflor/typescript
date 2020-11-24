8326bf65abf5342b303014474068cd285bbdf5b3
Sebastian Flor <sflor@telecomboutique.com>
2020-11-24 19:09:36 +0100

    feat: tsc --init and tsc -w

--- app.js
+++ app.js
@@ -1,13 +1,2 @@
-// UNKNOWN
-var userInput;
-var userName;
-userInput = 5;
-userInput = 'Max';
-if (typeof userInput === 'string')
-    userName = userInput;
-// NEVER
-function generateErr(message, code) {
-    throw { message: message, code: code };
-}
-var res = generateErr('An error occurred', 500);
-console.log(res);
+var username = 'Sebastian';
+console.log(username);
--- app.ts
+++ app.ts
@@ -1,17 +1,2 @@
-// UNKNOWN
-let userInput: unknown;
-let userName: string;
-
-userInput = 5;
-userInput = 'Max';
-
-if(typeof userInput === 'string')userName = userInput;
-
-// NEVER
-function generateErr(message: string, code: number): never {
-    throw {message, code};
-}
-
-const res = generateErr('An error occurred', 500);
-console.log(res);
-
+const username = 'Sebastian';
+console.log(username);
